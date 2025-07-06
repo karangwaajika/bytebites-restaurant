71d5868a25bd88d626c4254b8844bb644e877f88
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-07-06 19:19:12 +0200

    doc: set up auth-service project

---
+++ auth-service/src/main/java/com/lab/auth_service/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lab.auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+}
