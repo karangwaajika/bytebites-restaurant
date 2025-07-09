df74b01e4b1731c36a08fbc90fc7f575326e9e98
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-07-09 18:57:39 +0200

    feat: validate menu id before placing order

---
+++ order-service/src/main/java/com/lab/order_service/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.lab.order_service.controller;
+
+import com.lab.order_service.dto.OrderRequestDto;
+import com.lab.order_service.dto.OrderResponseDto;
+import com.lab.order_service.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api-order-service")
+public class OrderController {
+    OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/add")
+    @Operation(summary = "Add order",
+            description = "This request inserts a order to the database and returns " +
+                          "the inserted order ")
+    public ResponseEntity<OrderResponseDto> registerUser(
+            @RequestBody OrderRequestDto orderRequestDto
+    ) {
+        OrderResponseDto savedUser = this.orderService.order(orderRequestDto);
+        return ResponseEntity.ok(savedUser);
+    }
+}
--- order-service/src/main/java/com/lab/order_service/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/lab/order_service/service/impl/OrderServiceImpl.java
@@ -2,7 +2,9 @@ package com.lab.order_service.service.impl;
 
 import com.lab.order_service.dto.OrderRequestDto;
 import com.lab.order_service.dto.OrderResponseDto;
+import com.lab.order_service.exception.MenuNotFoundException;
 import com.lab.order_service.repository.OrderRepository;
+import com.lab.order_service.service.MenuService;
 import com.lab.order_service.service.OrderService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -11,15 +13,22 @@ import org.springframework.stereotype.Service;
 public class OrderServiceImpl implements OrderService {
     OrderRepository orderRepository;
     ModelMapper modelMapper;
+    MenuService menuService;
 
     public OrderServiceImpl(OrderRepository orderRepository,
-                            ModelMapper modelMapper) {
+                            ModelMapper modelMapper, MenuService menuService) {
         this.orderRepository = orderRepository;
         this.modelMapper = modelMapper;
+        this.menuService = menuService;
     }
 
     @Override
     public OrderResponseDto order(OrderRequestDto orderRequestDto) {
+        if (!menuService.isMenuExists(orderRequestDto.getMenuId())) {
+            throw new MenuNotFoundException(
+                    String.format("Menu with id '%s' doesn't exist",
+                            orderRequestDto.getMenuId()));
+        }
         return null;
     }
 }
