cfd9125f97a5d01d7b14aebaebe0afc0c1030153
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-07-13 22:46:50 +0200

    feat: add logs in the restaurant service

--- restaurant-service/src/main/java/com/lab/restaurant_service/service/OrderPlacedConsumer.java
+++ restaurant-service/src/main/java/com/lab/restaurant_service/service/OrderPlacedConsumer.java
@@ -3,7 +3,9 @@ package com.lab.restaurant_service.service;
 import com.lab.order_service.events.OrderPlacedEvent;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 public class OrderPlacedConsumer {
 
@@ -11,7 +13,7 @@ public class OrderPlacedConsumer {
     public void handleOrderPlaced(OrderPlacedEvent event) {
         System.out.println("Received order placed event in restaurant-service: " + event);
 
-        // TODO: Start preparing order, update DB etc.
+        log.debug("Event consumed successfully: {}", event);
     }
 }
 
